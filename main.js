e9c28c8031833720ee150624bdaddf37e1adf8ba
Oliver <draper87@gmail.com>
2020-06-09 16:18:48 +0200

    Release 2

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
 
   $('.prev i').click(
     function() {
-
+      muoviIndietro();
     }
   )
 
@@ -16,12 +16,36 @@ $(document).ready(function () {
 
 // devo creare una funzione che permetta al carousel di visualizzare l immagine successiva quando clicco la freccia destra
 function muoviAvanti() {
-  $('.images img.active').removeClass('active');
+  var immagineCorrente = $('.images img.active');
+  var dotBlue = $('.nav i.active');
 
-  $('.images img').next('img').addClass('active');
+  immagineCorrente.removeClass('active');
+  dotBlue.removeClass('active');
+
+  if (immagineCorrente.hasClass('last')) {
+    $('.images img.first').addClass('active');
+    $('.nav i.first').addClass('active');
+  }
+  else {
+    immagineCorrente.next('img').addClass('active');
+    dotBlue.next('i').addClass('active');
+  }
 }
 
 // devo creare una funzione che permetta al carousel di visualizzare l immagine precedente quando clicco la freccia sinistra
 function muoviIndietro() {
+  var immagineCorrente = $('.images img.active');
+  var dotBlue = $('.nav i.active');
 
+  immagineCorrente.removeClass('active');
+  dotBlue.removeClass('active');
+
+  if (immagineCorrente.hasClass('first')) {
+    $('.images img.last').addClass('active');
+    $('.nav i.last').addClass('active');
+  }
+  else {
+    immagineCorrente.prev('img').addClass('active');
+    dotBlue.prev('i').addClass('active');
+  }
 }
